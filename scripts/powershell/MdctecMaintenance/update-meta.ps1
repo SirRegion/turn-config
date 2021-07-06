25b059dc51a0b70c5c5983e5f246d043c6d77b94
qbuechner <quirin.buechner@mdctec.com>
2021-07-06 14:49:55 +0200

    fixed gitlab-ci job (2)

--- scripts/powershell/MdctecMaintenance/update-meta.ps1
+++ scripts/powershell/MdctecMaintenance/update-meta.ps1
@@ -1,2 +1,3 @@
-"$(git rev-parse --short HEAD)" | Out-File .\.meta\version
-"$(Get-Date -Format "dddd dd/MM/yyyy HH:mm K")" | Out-File .\.meta\timestamp
+New-Item -ItemType Directory -Force -Path .\.meta | Out-Null
+"$(git rev-parse --short HEAD)" | Out-File .\.meta\version -Force
+"$(Get-Date -Format "dddd dd/MM/yyyy HH:mm K")" | Out-File .\.meta\timestamp -Force
