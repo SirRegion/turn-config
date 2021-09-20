e4b57dfeff9a33c2be550108f2c56b9803d01252
qbuechner <quirin.buechner@mdctec.com>
2021-09-20 14:26:16 +0200

    MdctecMaintenanceMenu version 1.0.14

--- scripts/powershell-modules/MdctecMaintenanceMenu/menu/02_Setup_ComplianceBase/Install_ComplianceBase_with_docker/04_Launch_the_cb-app_container.ps1
+++ scripts/powershell-modules/MdctecMaintenanceMenu/menu/02_Setup_ComplianceBase/Install_ComplianceBase_with_docker/04_Launch_the_cb-app_container.ps1
@@ -1,1 +1,1 @@
-docker run -d -p 80:80/udp 80:80/tcp --name cb-app --rm complianceBaseContainerRegistry.azurecr.io/cb-app/stand-alone/windows:demo_2021.0.1
+docker run -d --rm -p 80:80/udp -p 80:80/tcp --name cb-app complianceBaseContainerRegistry.azurecr.io/cb-app/stand-alone/windows:demo_2021.0.1
