d6c433f8e5bf343b081ceb87999875c1f4d8507c
qbuechner <quirin.buechner@mdctec.com>
2021-10-04 16:03:03 +0200

    MMM version 1.0.21

--- scripts/powershell-modules/MdctecMaintenanceMenu/menu/02_ComplianceBase_Utilities/01_ComplianceBase_Installation/05_launch_the_cb-app_container.ps1
+++ scripts/powershell-modules/MdctecMaintenanceMenu/menu/02_ComplianceBase_Utilities/01_ComplianceBase_Installation/05_launch_the_cb-app_container.ps1
@@ -1,5 +1,5 @@
 docker run -d --rm `
-    -p 433:433/udp -p 433:433/tcp `
+    -p 443:443/udp -p 443:443/tcp `
     --mount 'type=volume,src=cb-app_data,dst=C:/app/data/mysql' `
     --name cb-app `
     complianceBaseContainerRegistry.azurecr.io/main/cb-app/mondi:0.0.1-rc2
