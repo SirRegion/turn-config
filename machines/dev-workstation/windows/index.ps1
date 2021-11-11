4ea973961b64fdc8d4ac4817b050cd423d511bb3
Quirin Büchner <quirin.buechner@mdctec.com>
2021-11-11 10:48:06 +0000

    Deleted machines/dev-workstation/windows/index.ps1

--- machines/dev-workstation/windows/index.ps1
+++
@@ -1,7 +0,0 @@
-# enable git autocompletion for powershell
-PowerShellGet\Install-Module posh-git -Scope CurrentUser -Force
-Add-PoshGitToProfile
-
-# enable docker autocompletion for powershell
-Install-Module DockerCompletion -Scope CurrentUser -Force
-Add-Content $PROFILE "`nImport-Module DockerCompletion"
