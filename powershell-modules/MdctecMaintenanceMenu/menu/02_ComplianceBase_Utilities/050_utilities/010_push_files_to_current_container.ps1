765312ae76d0179f8d44d54d6b77f9bfb1fc71e5
qbuechner <quirin.buechner@mdctec.com>
2022-02-18 11:40:17 +0100

    feat(MdctecMaintenanceMenu): release version 1.0.35

--- powershell-modules/MdctecMaintenanceMenu/menu/02_ComplianceBase_Utilities/050_utilities/010_push_files_to_current_container.ps1
+++ powershell-modules/MdctecMaintenanceMenu/menu/02_ComplianceBase_Utilities/050_utilities/010_push_files_to_current_container.ps1
@@ -1,26 +1,31 @@
 Param(
+# The path to the files to pull from the container relative to the C:/ directory
     [Parameter(Mandatory = $true)]
     [string]
-    $ContainerPath
+    $ContainerDirectoryPath
 )
 
 . "$Env:MMM_HOME/MdctecMaintenanceMenu/assets/environment/docker/default.env.ps1"
 
 $ErrorActionPreference = "Stop"
+$Env:CB_DOCKER_EXTRACT_PATH = [System.Environment]::GetEnvironmentVariable("CB_DOCKER_EXTRACT_PATH", "User")
 
+if (-Not $Env:CB_DOCKER_EXTRACT_PATH) {
+    Write-Warning "CB_DOCKER_EXTRACT_PATH variable not set :("
+    Write-Warning "Use the menu item 'pull files from current container'."
+    Write-Warning "You can also set this environment variable manually. (Scope: User)"
+    return
+}
 $ContainerStatus = $( docker inspect --format '{{ .State.Status }}' ($Env:CB_DOCKER_CONTAINER) )
 
 $ContainerStatusOK = -Not$LastExitCode -And ($ContainerStatus -eq 'running' -OR $ContainerStatus -eq 'exited')
 
-$CbHomeOK = $Env:CB_HOME -And (Test-Path $Env:CB_HOME)
-
 
 Write-Host
 Write-Host "OK:${ContainerStatusOK}`tContainerStatus = '$ContainerStatus'"
-Write-Host "OK:$CbHomeOK`tCB_HOME = '${Env:CB_HOME}'"
 Write-Host
 
-if ($ContainerStatusOK -And $CbHomeOK)
+if ($ContainerStatusOK)
 {
     if ($ContainerStatus -eq 'running') {
         Write-Host 'Stopping running container...'
@@ -32,22 +37,26 @@ if ($ContainerStatusOK -And $CbHomeOK)
         }
     }
 
+    # The path on the host where files are copied from
+    $HostPathPrefix = "$Env:CB_DOCKER_EXTRACT_PATH"
 
-    # The path to the files to pull from the container relative to the C:/ directory
-    $ContainerPath="etc"
-
-    # The path on the host where files are copied to
-    $HostPathPrefix="$Env:CB_HOME/environment/docker/cb-app/files"
-
+    $HostDirectoryPath = Join-Path $HostPathPrefix $ContainerDirectoryPath
     # make sure that the target files exist on the host
-    if (-Not (Test-Path "$HostPathPrefix/$ContainerPath/*")){
-        Write-Host "$HostPathPrefix/$ContainerPath is empty or does not exist" -Fore Red
+    if (-Not(Test-Path "$HostPathPrefix/$ContainerDirectoryPath/*")) {
+        Write-Host "$HostPathPrefix/$ContainerDirectoryPath is empty or does not exist" -Fore Red
         return
     }
 
     # now copy files from host to container
-    docker cp "$HostPathPrefix/$ContainerPath/." "${Env:CB_DOCKER_CONTAINER}:/$ContainerPath"
+    Write-Host "copying files from host to container"
+    ls "$HostDirectoryPath"
+    Write-Host
+    Write-Host "docker cp $HostDirectoryPath\. ${Env:CB_DOCKER_CONTAINER}:/$ContainerDirectoryPath"
+    docker cp "$HostDirectoryPath\." "${Env:CB_DOCKER_CONTAINER}:/$ContainerDirectoryPath"
+
+    Write-Host "docker start cb-app"
+    docker start cb-app
 }
-else{
+else {
     Write-Warning "Aborted!"
 }
