80651f130ad40d9458b42af8fc393296b179e44a
qbuechner <quirin.buechner@mdctec.com>
2022-02-21 17:53:33 +0100

    fix(MdctecMaintenanceMenu): fixed menu item 'run'

--- powershell-modules/MdctecMaintenanceMenu/menu/02_ComplianceBase_Utilities/010_run.ps1
+++ powershell-modules/MdctecMaintenanceMenu/menu/02_ComplianceBase_Utilities/010_run.ps1
@@ -37,10 +37,10 @@ function StartNew
     Write-Host "starting new container '$Env:CB_DOCKER_CONTAINER'"
 
     $CB_DOCKER_CONTAINER_ID = docker run -d `
-    -p "${Env:CB_DOCKER_PORT}:443/tcp" `
-    --mount 'type=volume,src=cb-app_data,dst=C:/app/data/mysql' `
-    --name "$ENV:CB_DOCKER_CONTAINER" `
-    "${Env:MTEC_PROD_REGISTRY}/main/${Env:CB_DOCKER_CONTAINER}/${Env:CB_VARIANT}:${Env:CB_VERSION}"
+        -p "${Env:CB_DOCKER_PORT}:443/tcp" `
+        --mount 'type=volume,src=cb-app_data,dst=C:/app/data/mysql' `
+        --name "$ENV:CB_DOCKER_CONTAINER" `
+        "${Env:MTEC_PROD_REGISTRY}/main/${Env:CB_DOCKER_CONTAINER}/${Env:CB_VARIANT}:${Env:CB_VERSION}"
 
     Write-Host "Container was launched with id $CB_DOCKER_CONTAINER_ID"
 }
@@ -56,6 +56,7 @@ Write-Host
 $ContainerStatus = docker inspect --format '{{ .State.Status }}' $ENV:CB_DOCKER_CONTAINER 2> $null
 if ($LastExitCode -gt 0){
     Write-Host "Could not find container named like '$Env:CB_DOCKER_CONTAINER'"
+    $ContainerStatusOK = $true
 }else{
     $ContainerStatusOK = $LastExitCode -gt 0 -OR $ContainerStatus -eq 'exited'
     Write-Host "ContainerStatus='$ContainerStatus', OK:${ContainerStatusOK}"
@@ -63,7 +64,7 @@ if ($LastExitCode -gt 0){
 
 Write-Host
 
-if (-Not"$ContainerStatusOK") { return }
+if (-Not "$ContainerStatusOK") { return }
 
 switch ($ContainerStatus) {
     'running' {
