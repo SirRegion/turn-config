2cf00b519638f86f1cd6c2d8e538459d8b330d75
qbuechner <quirin.buechner@mdctec.com>
2022-02-23 11:30:27 +0100

    chore: improved make targets related to push

--- variables.mk
+++ variables.mk
@@ -1,1 +1,2 @@
-export PROD_IMAGE_REGISTRY := complianceBaseContainerRegistry.azurecr.io
+export MTEC_PROD_REGISTRY := complianceBaseContainerRegistry.azurecr.io
+export MTEC_STAGE_REGISTRY := stage.mdctec.com:5000
