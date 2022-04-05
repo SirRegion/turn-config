3656d49af4f29f92df6ffd4a9d670d168adf9026
qbuechner <quirin.buechner@mdctec.com>
2022-04-05 18:55:58 +0200

    chore: update add-user.sh

--- scripts/bash/add-user.sh
+++ scripts/bash/add-user.sh
@@ -2,25 +2,28 @@
 
 read -p "NEW_USER_EMAIL:" NEW_USER_EMAIL
 NEW_USER_NAME=${$NEW_USER_EMAIL::6}
-echo "Your name on this machine will be: $NEW_USER_EMAIL"
+echo "Your name on this machine will be: $NEW_USER_NAME"
 
 read -p "Press enter to continue:"
 
-# Register the user, also enable sudo
+# Create the user
 useradd -s /bin/bash -d /home/"$NEW_USER_NAME" -m "$NEW_USER_NAME"
 
-#Configure groups
+# Configure groups
 for GROUP in sudo mtec docker
 do
   usermod -aG $GROUP "$NEW_USER_NAME"
 done
 
-su "$NEW_USER_NAME"
+# Configure Git
+su "$NEW_USER_NAME" -c git config --global user.name "${NEW_USER_NAME}"
+
+su "$NEW_USER_NAME" -c git config --global user.email "${NEW_USER_EMAIL}"
 
 # Create a password
 DEFAULT_PW="xWdNeQRv8R9Wp5b7"
-echo -e "${DEFAULT_PW}\n${DEFAULT_PW}" | passwd "$NEW_USER_NAME"
+echo -e "${DEFAULT_PW}\n${DEFAULT_PW}" | passwd "${NEW_USER_NAME}"
 
-# Force to reset password at first login
+# Force to reset the password at first login
 passwd --expire
 
