264ab7ac9519e549a89fc22968b615b8dbc044d7
Quirin Büchner <quirin.buechner@mdctec.com>
2022-04-06 12:48:33 +0000

    Add new file

---
+++ scripts/bash/install-make.apt.sh
@@ -0,0 +1,4 @@
+cd ~/Downloads
+wget -c http://archive.ubuntu.com/ubuntu/pool/main/m/make-dfsg/make_4.3-4ubuntu1_amd64.deb
+sudo apt-get install ./make_4.3-4ubuntu1_amd64.deb
+rm ./make_4.3-4ubuntu1_amd64.deb
