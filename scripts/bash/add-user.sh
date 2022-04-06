329fd8728c92f7ccc25f3b97d9bc1f7120156275
qbuechner <quirin.buechner@mdctec.com>
2022-04-06 11:18:20 +0200

    chore: improvements to add-user.sh

--- scripts/bash/add-user.sh
+++ scripts/bash/add-user.sh
@@ -1,23 +1,26 @@
 #!/bin/bash
 
 read -p "NEW_USER_EMAIL:" NEW_USER_EMAIL
-FULL_NAME=${NEW_USER_EMAIL/@*/}
-FIRST_NAME=${FULL_NAME/.*/}
-LAST_NAME=${FULL_NAME/*./}
+FULL_NAME="${NEW_USER_EMAIL/@*/}"
+FIRST_NAME=$(echo "${FULL_NAME/.*/}" | sed -re "s~(^)(.)~\U\2~g")
+LAST_NAME=$(echo "${FULL_NAME/*./}" | sed -re "s~(^)(.)~\U\2~g")
+FULL_NAME="${FIRST_NAME} ${LAST_NAME}"
 
-NEW_USER_NAME="${FIRST_NAME::1}${LAST_NAME::5}"
+NEW_USER_NAME=$(echo "${FIRST_NAME::1}${LAST_NAME::5}" | sed -re "s~(.)~\L\1~g")
+
+echo "The name for the new user will be '${NEW_USER_NAME}'"
+read -p "Press enter if you like it:"
 
 # Create the user
 useradd -s /bin/bash -d /home/"$NEW_USER_NAME" -m "$NEW_USER_NAME"
 
 # Configure groups
-for GROUP in sudo mtec docker
-do
+for GROUP in sudo mtec docker; do
   usermod -aG $GROUP "$NEW_USER_NAME"
 done
 
 # Configure Git
-su "$NEW_USER_NAME" -c git config --global user.name "${NEW_USER_NAME}"
+su "$NEW_USER_NAME" -c git config --global user.name "${FIRST_NAME} ${LAST_NAME}"
 
 su "$NEW_USER_NAME" -c git config --global user.email "${NEW_USER_EMAIL}"
 
