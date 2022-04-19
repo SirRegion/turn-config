5614b5a460447a98feb6bba28304923a4b11c489
qbuechner <quirin.buechner@mdctec.com>
2022-04-19 10:00:16 +0200

    feat: release 1.0.47
    use Docker image with tag 'latest'

--- powershell-modules/MdctecMaintenanceMenu/scripts/configure/set.ps1
+++ powershell-modules/MdctecMaintenanceMenu/scripts/configure/set.ps1
@@ -1,3 +1,4 @@
 [Environment]::SetEnvironmentVariable($args[0], $args[1], 'Machine')
 [Environment]::SetEnvironmentVariable($args[0], $args[1], 'Process')
 
+Write-Host "SetEnvironmentVariable($($args[0]),$($args[1]))"
